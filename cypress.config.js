c7dd502fb6545c54f79a784c901cb2a947345fb5
“Heisenberg” <ruku508501@gmail.com>
2024-04-09 21:28:25 -0400

    Removed reporter from e2e block.

--- cypress.config.js
+++ cypress.config.js
@@ -17,24 +17,6 @@ module.exports = defineConfig({
   e2e: {
     "video":false,
     "blockHosts":["www.google-analytics.com", "https://googleads.g.doubleclick.net/**/*"],
-    "reporter": "cypress-junit-reporter",
-    "reporterOptions": {
-      "mochaFile": "cypress/results/junit/test-results.[hash].xml",
-      "toConsole": true
-    },
-
-    "reporter": "cypress-multi-reporters",
-    "reporterOptions": {
-      "reporterEnabled": "mochawesome, junit",
-      "mochawesomeReporterOptions": {
-        "reportDir": "mochawesome-report",
-        "reportFilename": "report"
-      },
-      "junitReporterOptions": {
-        "mochaFile": "cypress/results/test-results.[hash].xml",
-        "toConsole": true
-      }
-    },
     setupNodeEvents(on, config) {
       //require("./cypress/support/dockerComoseGenerator")
       cypressSplit(on, config)
